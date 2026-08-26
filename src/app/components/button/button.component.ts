ec91c2b3392391a7381a1e9ef94862dbcbaffbbd
Simón Ibalo <simonibalo@gmail.com>
2026-08-26 20:32:33 -0300

    refactor: implement icon component at button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
+  imports: [IconComponent]
 })
 export class ButtonComponent {
   readonly label = input.required<string>();
