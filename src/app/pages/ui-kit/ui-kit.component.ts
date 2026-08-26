d4f4bfddb383eb5d69c4a0b53a0d7b3caa596421
Simón Ibalo <simonibalo@gmail.com>
2026-08-26 20:31:12 -0300

    feat: add icon component

--- src/app/pages/ui-kit/ui-kit.component.ts
+++ src/app/pages/ui-kit/ui-kit.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
+import { IconComponent } from "../../components/icon/icon.component";
 
 @Component({
   selector: 'app-ui-kit',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, IconComponent],
   templateUrl: './ui-kit.component.html',
   styleUrl: './ui-kit.component.css'
 })
