857d5972f3c6393c0727e8f53d003b9c883cf1c2
Simón Ibalo <simonibalo@gmail.com>
2026-08-28 16:14:04 -0300

    chore: remove ui-kit route and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,7 @@
 import { Routes } from '@angular/router';
-import { UiKitComponent } from './pages/ui-kit/ui-kit.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {
-        path: 'ui-kit',
-        component: UiKitComponent
-    },
     {
         path: '',
         component: HomeComponent
