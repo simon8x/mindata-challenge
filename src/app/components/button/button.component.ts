100556da502a652317452d8e5117347abd86faaa
Simón Ibalo <simonibalo@gmail.com>
2026-08-31 14:22:55 -0300

    fix: use anchor tags for navigation CTAs instead of buttons

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
 
 @Component({
@@ -10,4 +10,6 @@ import { IconComponent } from '../icon/icon.component';
 export class ButtonComponent {
   readonly label = input.required<string>();
   readonly variant = input<'primary' | 'text'>('primary');
+  readonly href = input<string>();
+  readonly isLink = computed(() => this.href() != null);
 }
